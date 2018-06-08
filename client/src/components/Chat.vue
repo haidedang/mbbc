04955501cbe18ebe424abdbde2def6e41dd23ad0
Hai Dang <haiduc.dang91@gmail.com>
2018-06-08 13:37:26 +0200

    implement websocket communication client side

--- client/src/components/Chat.vue
+++ client/src/components/Chat.vue
@@ -3,6 +3,7 @@
       <form id="form" >
             <v-text-field v-model="input" type="text"></v-text-field>
             <v-btn @click="submit" value="Send"></v-btn>
+            <v-text-field v-model="recipient" placeholder="recipient" type="text"></v-text-field>
         </form>
   <!--<router-view></router-view>-->
   </v-layout>
@@ -13,6 +14,7 @@ import {mapState} from 'vuex'
 import UserService from '@/services/UserService'
 import $ from 'jquery'
 import io from 'socket.io-client'
+import AuthService from "@/services/web3";
 
 let socket = null; 
 
@@ -20,6 +22,7 @@ export default {
   name: 'Profile',
   data () {
     return {
+      recipient:'',
       input:'', 
       address:'',
       userID:'',
@@ -33,20 +36,60 @@ export default {
     ])
   },
    beforeCreate: function() {
-   socket = io.connect('http://localhost:8081'); 
-   socket.emit('my other event', { my: 'dafaaf' });
-   socket.on('online', )
+    /*  console.log(this.user);
+   socket = io.connect('http://localhost:8081');
+   socket.on('connect', function() {
+     socket.emit('username', {username: this.user})
+  
+  console.log('Connected! ID: ' + socket.id);
+}); 
+
+   socket.on('online', function(data){
+     console.log('received')
+   } ) */
   },
+  created() { 
+     console.log('Here ' + this.user.userID);
+     let userName  = this.user.userID
+   socket = io.connect(this.user.storageAddress);
+   socket.on('connect', function() {
+     socket.emit('username', {username: userName})
+  
+  console.log('Connected! ID: ' + socket.id);
+}); 
+
+   socket.on('online', function(data){
+     console.log('received')
+   } )
+    socket.on('reply', function(data){
+      console.log(data); 
+    })
+  } ,
  
   methods: { 
     show() { 
       console.log(this.user);
     },
-    submit() { 
+    async submit() { 
         socket.emit('message', {
             userID: this.user, 
             sentTo: this.recipient, 
-            message: this.input});
+            message: this.input,
+            socket: socket.id});
+      console.log(this.recipient)
+      let url =  await AuthService.searchUser(this.recipient); 
+      console.log(url)
+        $.post(
+          url+'/test/'+this.recipient,
+          {
+            userID: this.user, 
+            sentTo: this.recipient, 
+            message: this.input},
+          response => {
+            console.log(response)
+          }
+        );
+        
     }
   }
 }

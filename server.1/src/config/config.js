aeb9d09cf043ce26d0bb27e057357ace2623d6cf
Hai Dang <haiduc.dang91@gmail.com>
2018-06-10 20:09:26 +0200

    modify env variables for build script

--- server.1/src/config/config.js
+++ server.1/src/config/config.js
@@ -1,6 +1,8 @@
 const config = {
-    mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/DropStore',
+    dbName: process.env.dbName,
+    mongoURL: 'mongodb://localhost:27017/'+ process.env.dbName || 'mongodb://localhost:27017/DropStore',
     port: process.env.PORT || 8081,
+    delay: process.env.DELAY,
     authentication: {
         jwtSecret: process.env.JWT_SECRET || 'secret'
       }

cf45975f9d81d0e8de01f96a6b68ae1653025300
Hai Dang <haiduc.dang91@gmail.com>
2018-06-10 21:11:23 +0200

    Deleting unncessary server2 file due to new build script

--- server.2/src/config/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/CryptoStorage',
-    port: process.env.PORT || 8082,
-    authentication: {
-        jwtSecret: process.env.JWT_SECRET || 'secret'
-      }
-  };
-
-module.exports =  config;

1c3864b78c9b93404127a4f7a1f175e2140318b3
Hai Dang <haiduc.dang91@gmail.com>
2018-06-13 22:07:36 +0200

    integrating User Controller  and Model

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,39 +1,66 @@
 const User = require('../models/User');
+const cuid = require('../models/User')
 
-exports.addContact = (req, res ) => { 
+exports.addContact = (req, res) => {
 
     // TODO: needs to be modified for different servers 
 
-         User.findOne({userID: req.params.currentUser})
-        .then((user)=> { 
-            if (user.contacts.includes(req.params.newContact)){
+    User.findOne({ userID: req.params.currentUser })
+        .then((user) => {
+            if (user.contacts.includes(req.params.newContact)) {
                 console.log('user already in contact List');
-                res.send('ERROR: user already in contact List'); 
-            }  else { 
-              user.contacts.push(req.params.newContact)
-              user.save();
-            } 
+                res.send('ERROR: user already in contact List');
+            } else {
+                user.contacts.push(req.params.newContact)
+                user.save();
+            }
         })
-        
-   /*  // newContact doesnt really need to be fetched, I just need the UserID
-   async function getNewContact(){ 
-    const newContact = await User.findOne({userID: req.params.newContact})
-    return newContact;
-   } */
+
     
-   /* (async () => {
-       Promise.all([getCurrentUser(),getNewContact()]).then(values => { 
-           let currentUser= values[0]; 
-           let newContact = values[1]; 
-           // save new Contact to Users 
-          if (currentUser.contacts.includes(newContact.userID)){
-              console.log('user already in contact List');
-              res.send('ERROR: user already in contact List'); 
-          }  else { 
-            currentUser.contacts.push(newContact.userID)
-            currentUser.save();
-          } 
-       })
-   })() */
-    
+
+
+    /*  // newContact doesnt really need to be fetched, I just need the UserID
+    async function getNewContact(){ 
+     const newContact = await User.findOne({userID: req.params.newContact})
+     return newContact;
+    } */
+
+    /* (async () => {
+        Promise.all([getCurrentUser(),getNewContact()]).then(values => { 
+            let currentUser= values[0]; 
+            let newContact = values[1]; 
+            // save new Contact to Users 
+           if (currentUser.contacts.includes(newContact.userID)){
+               console.log('user already in contact List');
+               res.send('ERROR: user already in contact List'); 
+           }  else { 
+             currentUser.contacts.push(newContact.userID)
+             currentUser.save();
+           } 
+        })
+    })() */
+
 }
+
+exports.addUser = (req, res) => {
+    const newUser = new User(req.body)
+    newUser.cuid = cuid()
+    newUser.save((err, saved) => {
+        if (err) {
+            res.status(500)
+                .send(err)
+        }
+        res.json({ User: saved })
+    })
+}
+
+exports.getUsers= (req, res) =>{
+    User.find()
+      .exec((err, Users) => {
+        if (err) {
+          res.status(500)
+            .send(err)
+        }
+        res.json({ Users: Users })
+      })
+  }
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -4,6 +4,7 @@ const AutoIncrement = require('mongoose-sequence')(mongoose);
 var Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    cuid: { type: 'String', required: true },
     userID: { type: 'String', required: true, unique: true },
     address: { type: 'String', required: true },
     storageAddress: {type: 'String'}, 

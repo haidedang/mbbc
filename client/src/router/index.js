ed6ba1ba7444fa89921ed5c7ffddfb55a3e839d9
Hai Dang <haiduc.dang91@gmail.com>
2018-06-13 14:06:47 +0200

    Improving Registering Flow

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -9,40 +9,74 @@ import Chat from '@/components/Chat'
 import Message from '@/components/Message'
 import Profile from '@/components/Profile'
 import Contacts from '@/components/FindContact'
+import RegisterDomain from '@/components/RegisterDomain'
+import Storage from '@/components/Storage'
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    { path: '/chat', name: 'chat', component: Chat },
-    { path: '/search', name: 'search', component: Search },
-    { path: '/registerDomain', name: 'domain', component: Domain },
-    { path: '/register', name: 'register', component: Register },
-    { path: '/login', name: 'login', component: Login },
-    { path: '/', name: 'home', component: Register },
-    {
-      path: '/profile/:id',
-      name: 'profile',
-      component: Profile
-    },
-    {
-      path: '/User/*',
-      name: 'user',
-      component: User
-    },
-    {
-      path: '/Message',
-      name: 'message',
-      component: Message
-    },
-    {
-      path: '/Contacts',
-      name: 'contacts',
-      component: Contacts
-    },
-    {
-      path: '*',
-      redirect: 'songs'
-    }
-  ]
+    routes: [
+        {
+            path: '/chat',
+            name: 'chat',
+            component: Chat
+        },
+        {
+            path: '/search',
+            name: 'search',
+            component: Search
+        },
+        {
+            path: '/registerDomain',
+            name: 'domain',
+            component: Domain
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: Register,
+            children: [
+                {
+                    path:'', component: RegisterDomain
+                },
+                {
+                    path:'storage', name:'storage', component: Storage
+                }
+            ]
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/',
+            name: 'home',
+            component: Register
+        },
+        {
+            path: '/profile/:id',
+            name: 'profile',
+            component: Profile
+        },
+        {
+            path: '/User/*',
+            name: 'user',
+            component: User
+        },
+        {
+            path: '/Message',
+            name: 'message',
+            component: Message
+        },
+        {
+            path: '/Contacts',
+            name: 'contacts',
+            component: Contacts
+        },
+        {
+            path: '*',
+            redirect: 'songs'
+        }
+    ]
 })

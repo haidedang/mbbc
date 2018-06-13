f5baeae10519266f7e60e7e0a977626cc78750e0
Hai Dang <haiduc.dang91@gmail.com>
2018-06-13 10:22:27 +0200

    modifying deployment script

--- client/migrations/2_deploy_contracts.js
+++ client/migrations/2_deploy_contracts.js
@@ -1,9 +1,14 @@
 
-const ENS = artifacts.require("./ENSRegistry.sol"); 
+const ENS = artifacts.require("./ENSRegistry.sol");
 const FIFSRegistrar = artifacts.require('./FIFSRegistrar.sol');
 const Resolver = artifacts.require('./URLResolver.sol');
+const ReverseResolver = artifacts.require('./DefaultReverseResolver.sol');
+const ReverseRegistrar = artifacts.require('./ReverseRegistrar.sol');
+const PublicResolver = artifacts.require('./PublicResolver.sol');
 
-const web3 = new (require('web3'))();
+const Web3 = require('web3');
+const provider = new Web3.providers.HttpProvider("http://localhost:9545");
+const web3 = new Web3(provider);
 const namehash = require('eth-ens-namehash');
 
 /**
@@ -12,10 +17,10 @@ const namehash = require('eth-ens-namehash');
  * @param {string} tld plain text tld, for example: 'eth'
  */
 function getRootNodeFromTLD(tld) {
-  return {
-    namehash: namehash.hash(tld),
-    sha3: web3.utils.sha3(tld)
-  };
+    return {
+        namehash: namehash.hash(tld),
+        sha3: web3.utils.sha3(tld)
+    };
 }
 
 /**
@@ -25,26 +30,41 @@ function getRootNodeFromTLD(tld) {
  * @param {string} tld tld which the FIFS registrar takes charge of
  */
 function deployFIFSRegistrar(deployer, tld) {
-  var rootNode = getRootNodeFromTLD(tld);
+    var rootNode = getRootNodeFromTLD(tld);
 
-  // Deploy the ENS first
-  deployer.deploy(ENS)
-    .then(()=> {
-      // deploy the Resolve and bind it with ENS 
-      return deployer.deploy(Resolver, ENS.address); 
-    })
-    .then(() => {
-      // Deploy the FIFSRegistrar and bind it with ENS
-      return deployer.deploy(FIFSRegistrar, ENS.address, rootNode.namehash);
-    })
-    .then(function() {
-      // Transfer the owner of the `rootNode` to the FIFSRegistrar
-      ENS.at(ENS.address).setSubnodeOwner('0x0', rootNode.sha3, FIFSRegistrar.address);
-    });
+
+    let registrar
+    // Deploy the ENS first
+
+   
+        deployer.deploy(ENS)
+            .then(() => {
+                // deploy the Resolve and bind it with ENS 
+                return deployer.deploy(Resolver, ENS.address);
+            })
+            .then(() => {
+                // deploy the ReverseResolver and bind it with ENS 
+                return deployer.deploy(ReverseResolver, ENS.address);
+            })
+            .then(function () {
+                return deployer.deploy(ReverseRegistrar, ENS.address, ReverseResolver.address)
+            })
+            .then((instance) => {
+                registrar = instance;
+                return deployer.deploy(PublicResolver, ENS.address)
+            })
+            .then(() => {
+                // Deploy the FIFSRegistrar and bind it with ENS
+                return deployer.deploy(FIFSRegistrar, ENS.address, rootNode.namehash);
+            })
+            .then(function () {
+                // Transfer the owner of the `rootNode` to the FIFSRegistrar
+                 ENS.at(ENS.address).setSubnodeOwner('0x0', rootNode.sha3, FIFSRegistrar.address); 
+            });
 }
 
-module.exports = function(deployer) {
-  var tld = 'eth'; 
-  deployFIFSRegistrar(deployer, tld); 
+module.exports = function (deployer) {
+    var tld = 'eth';
+    deployFIFSRegistrar(deployer, tld);
 };
 

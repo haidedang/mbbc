3dff6f19e195bcfa893c03768318e0f7517b94f5
Hai Dang <haiduc.dang91@gmail.com>
2018-06-13 19:21:43 +0200

    adding Tweet Controller

---
+++ server/src/controllers/TweetController.js
@@ -0,0 +1,92 @@
+const Tweet = require('../models/Tweet');
+const cuid = require('cuid')
+/**
+ * Get Tweets.
+ *
+ * @param req
+ * @param res
+ */
+exports.getTweets= (req, res) =>{
+    Tweet.find()
+      .exec((err, Tweets) => {
+        if (err) {
+          res.status(500)
+            .send(err)
+        }
+        res.json({ Tweets: Tweets })
+      })
+  }
+  
+  /**
+   * Finds Tweet by cuid.
+   *
+   * @param req
+   * @param res
+   */
+  exports.getTweet= (req, res) =>{
+    Tweet.findOne({ cuid: req.params.cuid })
+      .exec((err, Tweet) => {
+        if (err) {
+          res.status(500)
+            .send(err)
+        }
+        res.json({ Tweet: Tweet })
+      })
+  }
+  
+  
+  /**
+   * Finds Tweet by userId.
+   *
+   * @param req
+   * @param res
+   */
+  exports.getTweetByUser= (req, res) =>{
+    Tweet.findOne({ userId: req.params.userId })
+      .exec((err, Tweet) => {
+        if (err) {
+          res.status(500)
+            .send(err)
+        }
+        res.json({ Tweet: Tweet })
+      })
+  }
+  
+  /**
+   * Add Tweet.
+   *
+   * @param req
+   * @param res
+   */
+  exports.addTweet= (req, res) =>{
+    const newTweet = new Tweet(req.body)
+    newTweet.cuid = cuid()
+    newTweet.save((err, saved) => {
+      if (err) {
+        res.status(500)
+          .send(err)
+      }
+      res.json({ Tweet: saved })
+    })
+  }
+  
+  /**
+   * Delete Tweet.
+   *
+   * @param req
+   * @param res
+   */
+  exports.deleteTweet= (req, res) =>{
+    Tweet.findOne({ cuid: req.params.cuid })
+      .exec((err, Tweet) => {
+        if (err) {
+          res.status(500)
+            .send(err)
+        }
+  
+        Tweet.remove(() => {
+          res.status(200)
+            .end()
+        })
+      })
+  }

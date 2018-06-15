131414be7e9d0f473cf1123dce3329f516f864d1
Hai Dang <haiduc.dang91@gmail.com>
2018-06-15 02:18:01 +0200

    updating the store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,41 +1,42 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from 'vuex-persistedstate'
+import * as actions from './actions'
 
 Vue.use(Vuex)
 
+// strict: true
+const plugins =  [
+  createPersistedState()
+]
+
+const state =  {
+  token: null,
+  user: null,
+  isUserLoggedIn: false,
+  url: null
+}
+
+const mutations=  {
+  setToken (state, token) {
+    state.token = token
+    state.isUserLoggedIn = !!(token)
+  },
+  setUser (state, user) {
+    state.user = user
+  },
+  setURL(state, url) { 
+    state.url = url 
+  },
+  setProfile (state, payload) { 
+    state.user = payload.data.user 
+}
+}
+
+
 export default new Vuex.Store({
-  strict: true,
-  plugins: [
-    createPersistedState()
-  ],
-  state: {
-    token: null,
-    user: null,
-    isUserLoggedIn: false,
-    url: null
-  },
-  mutations: {
-    setToken (state, token) {
-      state.token = token
-      state.isUserLoggedIn = !!(token)
-    },
-    setUser (state, user) {
-      state.user = user
-    },
-    setURL(state, url) { 
-      state.url = url 
-    }
-  },
-  actions: {
-    setURL({commit}, url) {
-      commit('setURL', url)
-    },
-    setToken ({commit}, token) {
-      commit('setToken', token)
-    },
-    setUser ({commit}, user) {
-      commit('setUser', user)
-    }
-  }
+    state,
+    mutations, 
+    actions,
+    plugins
 })

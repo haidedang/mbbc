0bdbf5659f45df9a838cf4a74b4526858e3f6c70
Hai Dang <haiduc.dang91@gmail.com>
2018-06-16 22:15:57 +0200

    adding mutations

--- client/src/store/mutations.js
+++ client/src/store/mutations.js
@@ -1,14 +1,22 @@
-import Vue from 'vue' 
+import Vue from 'vue'
 
 
-export default { 
-    setUser (state, payload) { 
-        state.user = data
+export default {
+    setUser(state, response) {
+        state.user = response
+    },
+   /*  setContact(state, payload) {
+        state.user.contacts.push(payload.userID);
+    }, */
+    setToken(state, token) {
+        state.token = token
+        state.isUserLoggedIn = !!(token)
+    },
+    setURL(state, url) {
+        state.url = url
+    },
+    setProfile(state, response) {
+        state.profile = response
     }
 }
 
-export default { 
-    setContact (state, payload) { 
-        state.user.contacts.push(payload.userID); 
-    }
-}

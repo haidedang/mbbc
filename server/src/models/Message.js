1a6df35adbd15c7a9512839e87346eca0b025552
Hai Dang <haiduc.dang91@gmail.com>
2018-06-26 22:11:21 +0200

    adding friendrequest serverside

--- server/src/models/Message.js
+++ server/src/models/Message.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose'),
 
 const MessageSchema = new Schema({
   conversationId: {
-    type: Schema.Types.ObjectId,
+    type: Schema.Types.String,
     ref:'Conversation',
     required: true
   },
--- server/src/routes/authentication.routes.js
+++ server/src/routes/authentication.routes.js
@@ -16,7 +16,7 @@ module.exports = (app) => {
         AuthenticationController.authenticate)
     app.get('/guest/login/:MetaAddress', metaAuth, AuthenticationController.sign)
     app.get('guest/auth/:MetaMessage/:MetaSignature', metaAuth,
-    AuthenticationController.authenticateGuest)
+        AuthenticationController.authenticateGuest)
     app.post('/register', AuthenticationController.register)
     app.get('/users/:user', AuthenticationController.profile)
 
@@ -24,11 +24,21 @@ module.exports = (app) => {
 
     app.get('/users/:currentUser/:newContact', UserController.addContact)
 
+    // ------ FRIEND REQUEST --------- 
+
+    app.get('/friendRequest/:MetaAddress', metaAuth,
+        AuthenticationController.sign)
+    app.get('/friendRequest/auth/:userID/:recipient/:MetaMessage/:MetaSignature', metaAuth,
+        AuthenticationController.friendRequest)
+    app.post('/receiveFriendRequest/auth/:currentUser/:newContact/:MetaMessage/:MetaSignature', metaAuth,
+        AuthenticationController.receiveFriendRequest)
+    
+
     //-------- CHATTING ------------------------ 
     // TODO: Add Authentification for the selected User with unique JWT Token 
     app.get('/users/:userID/:conversationID', ConversationController.getConversations)
-    
+
     app.post('/conversation/:recipient', ConversationController.sendMessage)
     app.post('conversation/new/:recipient', ConversationController.newConversation)
-  
+
 }

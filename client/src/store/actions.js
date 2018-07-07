9aa2b0bb8b9663dd2513e7cf989398c90f322702
Hai Dang <haiduc.dang91@gmail.com>
2018-07-07 14:11:13 +0200

    Check if authenticated for messaging

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,83 +1,120 @@
 import UserService from '../services/UserService'
+import AuthentificationService from '../services/AuthenticationService'
 
 export const getProfile = ({ commit }, contact) => {
-    UserService.getUser(contact.url, contact.id)
-        .then((result) => {
-            console.log(result);
-            commit('setProfile', result.data.user) // result = new User! 
-        })
-}
-
-export const getBlogs = ({ commit }, contact) => {
-    UserService.getTweetsFromUser(contact.url)
-        .then((result) => {
-            console.log(result);
-            commit('setBlogs', result.data.tweets)
-        })
-}
-
-export const addContact = ({ commit }, contact) => {
-    console.log('DONE')
-    UserService.addContact(contact.url, contact.id, contact.recipient, contact.body)
-        .then((result) => {
-            console.log('Reached')
-            console.log('RESULT',result)
-            if (result == undefined) {
-                return;
-            } else {
-                commit('setContact', contact.recipient);
-            }
-        })
-}
-
-export const setCurrentConversation = ({ commit }, conversation) => {
-    return new Promise((resolve, reject) => {
-        UserService.getConversationByUserIDs(conversation.url, conversation.id, conversation.recipient)
-            .then((result) => {
-                commit('setCurrentConversation', result.data.conversation);
-                resolve(result.data.conversation)
-                /*  commit('setCurrentConversation', ) */
-            })
+  UserService.getUser(contact.url, contact.id)
+    .then((result) => {
+      console.log(result);
+      commit('setProfile', result.data.user) // result = new User!
     })
 }
 
+export const getBlogs = ({ commit }, contact) => {
+  UserService.getTweetsFromUser(contact.url)
+    .then((result) => {
+      console.log(result);
+      commit('setBlogs', result.data.tweets)
+    })
+}
+
+export const addContact = ({ commit }, contact) => {
+  console.log('DONE')
+  UserService.addContact(contact.url, contact.id, contact.recipient, contact.body)
+    .then((result) => {
+      console.log('Reached')
+      console.log('RESULT', result)
+      if (result == undefined) {
+        return;
+      } else {
+        commit('setContact', contact.recipient);
+      }
+    })
+}
+
+export const setCurrentConversation = ({ commit }, conversation) => {
+  return new Promise((resolve, reject) => {
+    UserService.getConversationByUserIDs(conversation.url, conversation.id, conversation.recipient)
+      .then((result) => {
+        commit('setCurrentConversation', result.data.conversation);
+        resolve(result.data.conversation)
+        /*  commit('setCurrentConversation', ) */
+      })
+  })
+}
+
 export const clearConversation = ({ commit }, conversation) => {
-    commit('clearConversation', conversation)
+  commit('clearConversation', conversation)
 }
 
 export const removeFriendRequest = ({ commit }, friendRequest) => {
-    commit('removeFriendRequest', friendRequest)
+  commit('removeFriendRequest', friendRequest)
 }
 
 export const setCurrentMessages = ({ commit }, conversation) => {
-    UserService.getMessagesByConversationId(conversation.url, conversation.id)
-        .then((result) => {
-             commit('setCurrentMessages', result) 
-        })
+  UserService.getMessagesByConversationId(conversation.url, conversation.id)
+    .then((result) => {
+      commit('setCurrentMessages', result)
+    })
 }
 
-export const clearMessages = ({commit}, messages) => { 
-    commit('clearMessages', messages)
+export const clearMessages = ({ commit }, messages) => {
+  commit('clearMessages', messages)
 }
 
 export const receiveFriendRequest = ({ commit }, friendRequest) => {
-    commit('receiveFriendRequest', friendRequest)
+  commit('receiveFriendRequest', friendRequest)
 }
 
 export const sendMessage = ({ commit }, message) => {
-    commit('addMessage', message)
+  commit('addMessage', message)
 }
 
 export const setURL = ({ commit }, url) => {
-    commit('setURL', url)
+  commit('setURL', url)
 }
 
 export const setToken = ({ commit }, token) => {
-    commit('setToken', token)
+  commit('setToken', token)
 }
 
 export const setUser = ({ commit }, user) => {
-    commit('setUser', user)
+  commit('setUser', user)
 }
 
+export const setContacts = ({ commit }, contact) => {
+  UserService.getContactsByUserId(contact.url, contact.userID)
+    .then((result) => {
+      console.log('setContacts', result)
+      commit('setContacts', result.data.Contacts)
+    })
+}
+
+export const clearContacts = ({commit}, contact) => {
+  commit('clearContacts', contact)
+}
+
+/**
+ *
+ * @param {*} param0
+ * @param {*} contact
+ *  Authenticate for Messaging and get a Token for that endpoint
+ */
+export const setChatToken =  ({ commit }, contact) => {
+  return new Promise((resolve, reject) => {
+   AuthentificationService.login(
+      contact.storageAddress,
+      `/guestAuth/${contact.name}/`,
+      '/guest/',
+      'POST',
+      contact
+    ).then(response => {
+        commit('setChatToken', response)
+        console.log(response)
+      })
+  })
+  // TOKEN RECEIVED, TO DO: Set the Chat TOken and authentication Flag
+  //  FOR Users of your OWN server no need for that Shit build that logic !
+
+  /* commit('setChatToken', contact) */
+}
 // store.dispatch('addContact')
--- client/src/store/mutations.js
+++ client/src/store/mutations.js
@@ -1,54 +1,75 @@
 import Vue from 'vue'
-/* import { setCurrentConversation } from './actions'; */
+/* import { setCurrentConversation, clearContacts } from './actions'; */
 
 
 export default {
-    setUser(state, response) {
-        state.user = response
-    },
-    setContact(state, payload) {
-        state.user.contacts.push(payload);
-    },
-    setToken(state, token) {
-        state.token = token
-        state.isUserLoggedIn = !!(token)
-    },
-    setURL(state, url) {
-        state.url = url
-    },
-    setBlogs(state, response) {
-        state.blogs = response;
-    },
-    setProfile(state, response) {
-        state.profile = response
-    },
-    setCurrentConversation(state, response) {
-        state.conversation = response
-    },
-    setCurrentMessages(state, response) {
-        console.log(response)
-        response.data.message.forEach( (message) =>{
-            state.messages.push(message)
-        })
+  setUser(state, response) {
+    state.user = response
+  },
+  setContact(state, payload) {
+    state.user.contacts.push(payload);
+  },
+  setToken(state, token) {
+    state.token = token
+    state.isUserLoggedIn = !!(token)
+  },
+  setURL(state, url) {
+    state.url = url
+  },
+  setBlogs(state, response) {
+    state.blogs = response;
+  },
+  setProfile(state, response) {
+    state.profile = response
+  },
+  setCurrentConversation(state, response) {
+    state.conversation = response
+  },
+  setCurrentMessages(state, response) {
+    console.log(response)
+    response.data.message.forEach((message) => {
+      state.messages.push(message)
+    })
 
-    },
-    clearConversation(state, response){
-        state.conversation  = response
-    },
-    clearMessages(state, messages){ 
-        state.messages = []
-    },
-    receiveFriendRequest(state, response){ 
-        state.friendRequests.push(response); 
-    },
-    removeFriendRequest(state,response){ 
-      let newArray=  state.friendRequests.filter((friendRequest)=>{ 
-            friendRequest.sender !== response.sender
-        })
-    state.friendRequests = newArray; 
-    },
-    addMessage(state, message) {
-        state.messages.push(message);
-    }
+  },
+  clearConversation(state, response) {
+    state.conversation = response
+  },
+  clearMessages(state, messages) {
+    state.messages = []
+  },
+  receiveFriendRequest(state, response) {
+    state.friendRequests.push(response);
+  },
+  removeFriendRequest(state, response) {
+    let newArray = state.friendRequests.filter((friendRequest) => {
+      friendRequest.sender !== response.sender
+    })
+    state.friendRequests = newArray;
+  },
+  addMessage(state, message) {
+    state.messages.push(message);
+  },
+  setContacts(state, response) {
+    console.log('beginning of setting contacts', response)
+    let val = false;
+    let distinctURL = []
+    response.forEach(contact => distinctURL.push(contact.storageAddress));
+    let unique = [...new Set(distinctURL)];
+    unique.forEach(contact => {
+      state.endpoints.push({ endpoint: contact, token: '', authenticated: false });
+    })
+    state.contacts = response;
+    console.log('new endpoints Array', state.endpoints);
+  },
+  setChatToken(state, response) {
+    let result = state.endpoints.filter((contact) => contact.endpoint == response.user.storageAddress)[0]
+    result.authenticated = true;
+    result.token = response.token;
+    state.currentEndpoint = result;
+  },
+  clearContacts(state, response) {
+    state.endpoints = response;
+  }
 }
 

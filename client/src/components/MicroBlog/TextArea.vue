9ed3a581385d02df1bf0e41a0db9bc0fcf8d32d1
Hai Dang <haiduc.dang91@gmail.com>
2018-07-12 13:47:00 +0200

    fix Blog appearance bug

--- client/src/components/MicroBlog/TextArea.vue
+++ client/src/components/MicroBlog/TextArea.vue
@@ -3,40 +3,41 @@
         <v-flex>
           <div class="text">
             <textarea placeholder="Blog Here .." autofocus v-model="input" @keyup.enter.native="postBlog"></textarea>
-         </div>    
+         </div>
         </v-flex>
     </v-layout>
 </template>
 
 <script>
 export default {
-    data() {
-        input: ''
-    },
-    methods: {
-        async postBlog(event) {
-            event.preventDefault();
-            console.log("You Pressed Enter")
-            //this.sendTweet(this.input)
-            //this.input=''
-        }
+  data() {
+    return {
+      input: ""
+    };
+  },
+  methods: {
+    async postBlog(event) {
+      event.preventDefault();
+      console.log("You Pressed Enter");
+      //this.sendTweet(this.input)
+      //this.input=''
     }
-
-}
+  }
+};
 </script>
 <style lang="less">
-  .text {
-    height: 160px;
-    margin-bottom:1vh;
+.text {
+  height: 160px;
+  margin-bottom: 1vh;
   textarea {
     padding: 2vh;
     height: 100%;
     width: 70%;
     border-style: solid;
     border-width: 2px;
-    border-color: #43A047;
+    border-color: #43a047;
     outline: none;
     resize: none;
   }
-  }
+}
 </style>
